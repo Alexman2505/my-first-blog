bc045aac855a7b2a5b59757da1248840675e2ec1
Alexman2505 <alexman2505@gmail.com>
2023-01-27 19:35:33 +0300

    i add redirect in views.py. all is working now

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -6,7 +6,7 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
+        published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
